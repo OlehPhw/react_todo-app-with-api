3096a3e74045d1fc290f3d0c8fe2f173a4f2b9b8
oleh <olehphw@gmail.com>
2024-08-23 11:29:13 +0300

    added Todo app with full functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { getTodos, USER_ID } from './api/todos';
+import { Todo } from './types/Todo';
+import cn from 'classnames';
+import { FilterMethods } from './types/FilterMethods';
+import { wait } from './utils/fetchClient';
+import { Header } from './components/Header/Header';
+import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [activeFilter, setActiveFilter] = useState(FilterMethods.All);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [loadingTodosId, setLoadingTodosId] = useState<number[]>([]);
+  const [editingTodo, setEditingTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const fetchedTodos = await getTodos();
+
+        setTodos(fetchedTodos);
+      } catch (error) {
+        setErrorMessage('Unable to load todos');
+      }
+    };
+
+    fetchTodos();
+  }, [setErrorMessage, setTodos]);
+
+  useEffect(() => {
+    if (errorMessage) {
+      wait(3000).then(() => setErrorMessage(''));
+    }
+  }, [errorMessage]);
+
+  const filteredTodos = useMemo(() => {
+    return todos.filter(todo => {
+      switch (activeFilter) {
+        case FilterMethods.All:
+          return todo;
+        case FilterMethods.ACTIVE:
+          return !todo.completed;
+        case FilterMethods.COMPLETED:
+          return todo.completed;
+        default:
+          return todo;
+      }
+    });
+  }, [todos, activeFilter]);
+
+  const handleOnClickHideError = () => setErrorMessage('');
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          setErrorMessage={setErrorMessage}
+          setTempTodo={setTempTodo}
+          setTodos={setTodos}
+          todos={todos}
+          errorMessage={errorMessage}
+          setLoadingTodosId={setLoadingTodosId}
+          loadingTodosId={loadingTodosId}
+          editingTodo={editingTodo}
+        />
+
+        <section className="todoapp__main" data-cy="TodoList">
+          <TodoList
+            setTodos={setTodos}
+            setErrorMessage={setErrorMessage}
+            filteredTodos={filteredTodos}
+            todos={todos}
+            tempTodo={tempTodo}
+            setLoadingTodosId={setLoadingTodosId}
+            loadingTodosId={loadingTodosId}
+            setEditingTodo={setEditingTodo}
+            editingTodo={editingTodo}
+          />
+        </section>
+        <Footer
+          todos={todos}
+          activeFilter={activeFilter}
+          setActiveFilter={setActiveFilter}
+          setLoadingTodosId={setLoadingTodosId}
+          setTodos={setTodos}
+          setErrorMessage={setErrorMessage}
+        />
+      </div>
+      <div
+        data-cy="ErrorNotification"
+        className={cn(
+          'notification',
+          'is-danger',
+          'is-light',
+          'has-text-weight-normal',
+          { hidden: !errorMessage },
+        )}
+      >
+        <button
+          data-cy="HideErrorButton"
+          type="button"
+          className="delete"
+          onClick={handleOnClickHideError}
+        />
+        {errorMessage}
+      </div>
+    </div>
   );
 };
